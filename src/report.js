d422b8bf12fdde61fa68e490c395564d0ae16439
ikjae1002 <ij374@nyu.edu>
2017-09-28 14:44:08 -0400

    updating report.js with file inputs

---
+++ src/report.js
@@ -0,0 +1,45 @@
+// report.js
+// Programer: Ikjae Jung
+// Date 9/28
+
+const fs = require('fs');
+
+let result;
+
+function content(callback){
+    fs.readFile("business.json", "utf8", (err, data) => {
+        if(err){
+            return callback(err);
+        }callback(null, data);
+        // const elements = data.split("\n");
+        // const array = [];
+        // let f = JSON.parse(elements[0]);
+        // for(let i = 0; i < elements.length; i++){
+        //     const objjson = JSON.parse(elements[i]);
+        //     array.push(objjson);
+        // }
+        // console.log(array[0]);
+        // console.log(array[0].name + 'is a business located in ' + array[0].city + ' with a rating of ' + array[0].stars + ' and ' + array[0].review_count + ' reviews.');
+        //console.log(array[0][hours]);
+        //console.log(array);
+        // content = array
+    });
+}
+
+content(function(err,data){
+    const elements = data.split("\n");
+    const array = [];
+    let f = JSON.parse(elements[0]);
+    for(let i = 0; i < elements.length; i++){
+        const objjson = JSON.parse(elements[i]);
+        array.push(objjson);
+    }
+    // console.log(array[0]);
+    // console.log(array[0].name + 'is a business located in ' + array[0].city + ' with a rating of ' + array[0].stars + ' and ' + array[0].review_count + ' reviews.');
+    //console.log(array[0][hours]);
+    //console.log(array);
+    result = array
+    console.log(result[0]);
+});
+
+console.log(result);
